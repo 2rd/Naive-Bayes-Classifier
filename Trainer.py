cdc74f50932d1a1ccf0af166e4ace02ba196253e
Tord Kvifte <tord@leli.no>
2018-03-16 15:50:02 +0100

    Kan nå lage vokabularer med sannsynligheten for hvert ord, gitt klasse(positiv eller negativ)

--- Trainer.py
+++ Trainer.py
@@ -6,8 +6,12 @@
 
 
 def main():
+    import os
+
     posPath = 'C:\\Users\\tordk\\Documents\\INFO284\\1rstGroupAssingment\\Project\\DATA\\aclImdb\\train\\pos'
     negPath = 'C:\\Users\\tordk\\Documents\\INFO284\\1rstGroupAssingment\\Project\\DATA\\aclImdb\\train\\neg'
+    #posPath = 'C:\\Users\\tordk\\Documents\\INFO284\\1rstGroupAssingment\\300_pos'
+    #negPath = 'C:\\Users\\tordk\\Documents\\INFO284\\1rstGroupAssingment\\300_neg'
 
     posWords = wordParser(posPath)
     negWords = wordParser(negPath)
@@ -17,9 +21,16 @@ def main():
 
     numOfPosFiles = len([file for file in os.listdir(posPath)])
     numOfNegFiles = len([file for file in os.listdir(negPath)])
+    probOfClass1 = numOfPosFiles/(numOfNegFiles + numOfPosFiles)
+    probOfClass0 = numOfNegFiles/(numOfNegFiles + numOfPosFiles)
 
     posWordProbability = wordProbability(posVocab, posWords.__len__())
+    negWordProbability = wordProbability(negVocab, negWords.__len__())
 
+    print('antall ganger bad i positiv: ' + str(posVocab.get('bad')))
+    print('sannsynlighet for bad i positiv: ' + str(posWordProbability.get('bad')))
+    print('antall ganger bad i negativ: ' + str(negVocab.get('bad')))
+    print('sannsynlighet for bad i negativ: ' + str(negWordProbability.get('bad')))
 
 # Parser alle dokumentene i en mappe og returnerer en liste med alle ordene de inneholder.
 def wordParser(path):
@@ -57,6 +68,7 @@ def vocabulary(words):
 def wordProbability(vocabulary, totalWords):
     newVocab = dict()
     for word in vocabulary:
-        newVocab[word] = dict.get(word)/totalWords
+
+        newVocab[word] = vocabulary.get(word)/totalWords
 
     return newVocab
